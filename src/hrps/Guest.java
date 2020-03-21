cc6c060b57cfd3157ed5f4cca189ed553a8d269d
Mohamad Asyraaf <masyraaf@hotmail.com>
2020-03-21 20:20:27 +0800

    Updated guest and room

--- src/hrps/Guest.java
+++ src/hrps/Guest.java
@@ -1,5 +1,76 @@
 package hrps;
 
 public class Guest {
-
+	private String id;
+	private String name;
+	private String creditCardDetails;
+	private String address;
+	private String country;
+	private String gender;
+	private String nationality;
+	private int contact;
+	
+	public Guest(String id, String name, String creditCardDetails, String address, String country, String gender, String nationationality, int contact) {
+		this.id = id;
+		this.name = name;
+		this.creditCardDetails = creditCardDetails;
+		this.address = address;
+		this.country = country;
+		this.gender = gender;
+		this.nationality = nationality;
+		this.contact = contact;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCreditCardDetails() {
+		return creditCardDetails;
+	}
+	public void setCreditCardDetails(String creditCardDetails) {
+		this.creditCardDetails = creditCardDetails;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public int getContact() {
+		return contact;
+	}
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+	
+	
+	
+	
 }
