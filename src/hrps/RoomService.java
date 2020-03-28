e87af7b25e38dd391045cd2bdd6226c13252e6ad
Muhazerin <muhd.muhazerin@gmail.com>
2020-03-28 23:06:08 +0800

    added some minor changes

--- src/hrps/RoomService.java
+++ src/hrps/RoomService.java
@@ -1,42 +1,36 @@
 package hrps;
 
-//a) Hotel staff can order room service meals on guest’s behalf upon his/her request.
+//a) Hotel staff can order room service meals on guests behalf upon his/her request.
 //b) List of menu items selection will be displayed for selection.
 //c) Each menu item will have a name, a description of how it is prepared and price. (Under MenuItem class)
 //d) When ordered, there will be a room service order created with a date/time, remarks (eg, less oil, less
 //salt) and the status (confirmed, preparing, delivered).
 //e) The order status will be updated accordingly. ???
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class RoomService {
 	
-	private MenuItem[] menuItem;
+	private ArrayList<MenuItem> menuItem;
 	
 	private enum orderStatus {
 		CONFIRMED, PREPARING, DELIVERED; 
 	}
 	
-	private Date dateTime =  null;
+	private LocalDate dateTime =  null;
 	
 	private String remarks;
 //	private String roomId;
 	
 	public RoomService() {
-		this.menuItem = new MenuItem[99];
+		this.menuItem = new ArrayList<MenuItem>();
 		this.remarks = "";
-	}
-	
-	public String listMenuItems() {
-		for(int i = 0; i<menuItem.length; i++) {
-			menuItem[i].printOrder;
-		}
 	}
 	
 	public void placeOrder(String roomId, int menuId, String rmks) {
 		//get date and time
-		this.dateTime = new Date();
+		this.dateTime = LocalDate.now();
 		
 		System.out.println("Room: " + roomId);
 		System.out.println("Ordered: " + menuItem[menuId-1].getName());
@@ -52,8 +46,6 @@ public class RoomService {
 	}
 	
 	public float getMenuItemsTotalCost() {
-		
-
 		return 0;
 	}
 	
